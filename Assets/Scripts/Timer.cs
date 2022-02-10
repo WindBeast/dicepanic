e40ffec5b5b20be73501a8816d7a0068a5cf9864
Wind_Beast_legion <daichi.kamada.121213@gmail.com>
2022-02-10 14:53:52 +0900

    add countdown and audio

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -5,43 +5,97 @@ using UnityEngine.UI;
 
 public class Timer : MonoBehaviour
 {
-    public Text leftTime;
-    public Image timeGauge; 
-    float countTime = 0;
-    float left = 0;
-    bool loop = true;
+    [SerializeField] AudioClip cnt;
+    [SerializeField] AudioClip bgm;
+    [SerializeField] AudioSource cntSource;
+    [SerializeField] AudioSource bgmSource;
+    float countTime = 0; // 時間をカウントするための汎用変数
+    int countDownNum = 0; // カウントダウン(3,2,1,0)が入る
+    public GameObject countDownPanel; // ゲーム開始カウントダウンのパネル
+    public Text countDownText; // ゲーム開始カウントダウンのテキスト
+    public Image countDownGauge; // ゲーム開始カウントダウンのゲージ
+    int timeLimit = 180;
+    float left = 0; // 残り時間を扱うための変数
+    public Text leftTime; // 残り時間のテキスト
+    public Image timeGauge; // 残り時間のゲージ
+    public bool isCount = false; // countTimeを進めていいかどうかのフラグ
+    public bool isBeforeStart = false; // ゲーム開始カウントダウン中かどうかのフラグ
+    public bool isGameStart = false; // ゲーム中かどうかのフラグ
     // Start is called before the first frame update
     void Start()
     {
-        
+        countDownNum = 3;
+        countDownText.text = "3";
+        countDownPanel.SetActive(false);
+        leftTime.text = timeLimit.ToString();
     }
 
     // Update is called once per frame
     void Update()
     {
-        // countTimeに、ゲームが開始してからの秒数を格納
-        if(loop)
+        if (Input.GetKeyDown (KeyCode.Space)) 
         {
-            countTime += Time.deltaTime;
-            left = 180 - countTime;
+            if(countDownNum == 3)
+            {
+                isCount = true;
+                isBeforeStart = true;
+                countDownPanel.SetActive(true);
+                cntSource.PlayOneShot(cnt);
+            }
         }
 
-        // 小数2桁にして表示
-        if(left >= 60)
+        if(isCount)
         {
-            leftTime.text = left.ToString("F0");
-            timeGauge.fillAmount = left / 180;
+            countTime += Time.deltaTime;
         }
-        else if(left >= 0)
+
+        if(isBeforeStart)
         {
-            leftTime.text = left.ToString("F2");
-            timeGauge.fillAmount = left / 180;
+            if(countTime < 1)
+            {
+                countDownGauge.fillAmount = 1 - countTime;
+            }
+            else
+            {
+                if(countDownNum == 1)
+                {
+                    countTime = 0;
+                    countDownNum--;
+                    isBeforeStart = false;
+                    isGameStart = true;
+                    countDownPanel.SetActive(false);
+                    bgmSource.PlayOneShot(bgm);
+                }
+                else
+                {
+                    countTime = 0;
+                    countDownGauge.fillAmount = 1;
+                    countDownNum--;
+                    countDownText.text = countDownNum.ToString();
+                }
+            }
         }
-        else
+
+        if(isGameStart)
         {
-            leftTime.text = "00.00";
-            timeGauge.fillAmount = 0;
-            loop = false;
+            left = timeLimit - countTime;
+            if(left >= 60)
+            {
+                leftTime.text = Mathf.Floor(left).ToString("F0");
+                timeGauge.fillAmount = left / timeLimit;
+            }
+            else if(left >= 0)
+            {
+                leftTime.text = left.ToString("F2");
+                timeGauge.fillAmount = left / timeLimit;
+            }
+            else
+            {
+                leftTime.text = "00.00";
+                timeGauge.fillAmount = 0;
+                isCount = false;
+                isGameStart = false;
+            }
         }
     }
 }
